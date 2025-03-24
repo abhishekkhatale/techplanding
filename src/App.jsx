459088f848635a505a90d829cc3721ee40fa395f
abhishekkhatale <abhishekkhatale007@gmail.com>
2025-03-24 13:55:05 +0530

    uyuyuuy

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Feature from './components/Feature'
+import Steps from './components/Steps'
+import Review from './components/rEVIEW.JSX'
+import Footer from './components/Footer'
+
+
+function App() {
+  return (
+    <>
+    <Hero/>
+    <Feature/>
+    <Steps/>
+    <Review/>
+    <Footer/>
+    </>
+    
+    
+  )
+}
+
+export default App
